c447ff425e3db6de539faf7d26bb51c2b60f3e63
Exlovering <151601191+Exlovering@users.noreply.github.com>
2023-11-21 16:36:30 +0700

    change port to PORT

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")))
 
 app.get("/", (req,res) => {
 
-    res.send('Hello borntoDev Co., Ltd.');
+    res.send('Hello borntoDev1111 Co., Ltd.');
 
 })
 
-app.listen(port, ()=>{
-    debug("Listening on port" + chalk.red(" : "+port));
+app.listen(PORT, ()=>{
+    debug("Listening on port" + chalk.red(" : "+PORT));
 })
